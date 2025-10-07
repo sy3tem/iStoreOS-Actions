981a65f6ab343a626f1a2f0a8966034b95a62076
sy3tem <sy3tem@qq.com>
2025-10-07 22:22:52 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -3,8 +3,8 @@
 # ============= 若启用 则打开注释 ============================
 
 # 分区扩容 by sirpdboy 
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp"
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-partexp-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-partexp-zh-cn"
 # 酷猫主题 by sirpdboy 
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-kucat"
 # 进阶设置 by sirpdboy 
@@ -18,13 +18,33 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-mosdns"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-mosdns-zh-cn"
 # Turbo ACC 网络加速
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc"
 # 应用过滤 openappfilter.com
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-appfilter"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-appfilter-zh-cn"
 # 设置向导 by sirpdboy
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netwizard"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-netwizard-zh-cn"
+
+# ============= 添加MT76无线驱动和网络组件 ==================
+# MT76系列无线驱动
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt76-core"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt76-connac"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt7915e"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-mt7916-firmware"
+# 交换机驱动
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-swconfig"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-switch-rtl8366-smi"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-switch-rtl8367b"
+# 虚拟以太网驱动
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-veth"
+# iptables nfq相关组件
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES iptables-mod-nfqueue"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES kmod-ipt-nfqueue"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES iptables-mod-filter"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES iptables-mod-conntrack-extra"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES iptables-mod-ipopt"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES iptables-mod-nat-extra"
 
 # ============= 若去除组件 则打开注释 ============================
 # 若去掉istore商店 则打开注释
